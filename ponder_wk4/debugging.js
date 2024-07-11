0f61242d8393d69e00fc880d45d2608005b11dc3
Christopher Hylton <jamboyh5@gmail.com>
2024-07-10 21:09:55 -0600

    debugging

--- ponder_wk4/debugging.js
+++ ponder_wk4/debugging.js
@@ -1,6 +1,11 @@
-const pi = 3.14;
-const radius = 3;
+const PI = 3.14;
 let area = 0;
-area = radius * radius * pi;
+function circleArea(radius) {
+  const area = radius * radius * PI;
+  return area;
+}
+area = circleArea(3)
+console.log("Area1:", area);
 radius = 4;
-area = radius * radius * pi;
+area = circleArea(4)
+console.log("Area2:", area)
